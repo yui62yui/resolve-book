eda02def5d5bd01ed8c85505dd6882a8e08d18d9
yui62yui <you@example.com>
2023-08-08 11:39:04 +0900

    Feat / 정보 불러와지지 않는 오류로 인해 브랜치 재생성 및 기본세팅 재설정 #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
-import Router from "./shared/Router";
+import React from 'react';
+import Router from './shared/Router';
+import axios from 'axios';
 
 const App = () => {
+  const fetchPosts = async () => {
+    const { data } = await axios.get('http://localhost:4000/test');
+  };
   return <Router />;
 };
 
