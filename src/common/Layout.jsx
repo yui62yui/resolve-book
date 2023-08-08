e635a90f8841025182693ab3f7baddf26b06f7e0
sua <choisua98@gmail.com>
2023-08-08 19:31:57 +0900

    Feat/커뮤니티기능구현, 데이터받아오기, 모달구현#3

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import backgroundImg from '../assets/images/bg.png';
+import styled from 'styled-components';
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <BackgroundContainer>
+        <Outlet />
+      </BackgroundContainer>
     </>
   );
 };
 
 export default Layout;
+
+const BackgroundContainer = styled.div`
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  width: 100%;
+  min-height: 100vh;
+  height: auto;
+  padding: 100px 50px;
+  box-sizing: border-box;
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Post from '../pages/Post';
 import Saved from '../pages/Saved';
 import Layout from '../common/Layout';
+import Community from '../pages/Community';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Post />} />
           <Route path="/:id" element={<Saved />} />
+          <Route path="/community" element={<Community />} />
         </Route>
       </Routes>
     </BrowserRouter>
