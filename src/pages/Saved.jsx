64ec3ccbecf3b11b2cffbb1fe8dfc9da37ff6fe9
yui62yui <you@example.com>
2023-08-08 16:06:59 +0900

    Feat,Style / 버튼 클릭 시 활성화 기능 및 css 추가

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import MyPage from '../components/MyPage';
 import SavedPage from '../components/SavedPage';
 
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 const Saved = () => {
   const [data, setData] = useState([]);
   const [myButton, setMyButton] = useState(true);
@@ -17,22 +20,38 @@ const Saved = () => {
     setMyButton(true);
   }, []);
 
+  const buttonActiveHandler = (e) => {
+    const btns = document.querySelectorAll('button');
+    btns.forEach((btn) => {
+      if (e.currentTarget === btn) {
+        btn.classList.add(`active`);
+      } else {
+        btn.classList.remove(`active`);
+      }
+    });
+    console.log(e.currentTarget);
+  };
+
   return (
     <Container>
-      <PageButton
-        onClick={() => {
-          setMyButton(true);
-        }}
-      >
-        나의 글 모아보기
-      </PageButton>
-      <PageButton
-        onClick={() => {
-          setMyButton(false);
-        }}
-      >
-        보관한 글 모아보기
-      </PageButton>
+      <Stack direction="row" spacing={2} sx={{ display: 'block', margin: '30px auto 100px' }}>
+        <PageButton
+          onClick={(event) => {
+            setMyButton(true);
+            buttonActiveHandler(event);
+          }}
+        >
+          나의 글 모아보기
+        </PageButton>
+        <PageButton
+          onClick={(event) => {
+            setMyButton(false);
+            buttonActiveHandler(event);
+          }}
+        >
+          보관한 글 모아보기
+        </PageButton>
+      </Stack>
       {myButton === true ? <MyPage /> : <SavedPage />}
     </Container>
   );
@@ -41,12 +60,35 @@ const Saved = () => {
 export default Saved;
 
 const Container = styled.div`
+  box-sizing: border-box;
+
+  width: 100%;
+  margin: 0 auto;
+
+  text-align: center;
+
   color: #999;
-  font-size: 100px;
 `;
 
 const PageButton = styled.button`
-  width: 180px;
+  width: 200px;
   height: 50px;
-  color: black;
+
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: -1px;
+
+  border-radius: 25px;
+  background-color: #fff;
+  cursor: pointer;
+
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #edeacb;
+  }
+
+  &.active {
+    background-color: #d4cc7c;
+  }
 `;
