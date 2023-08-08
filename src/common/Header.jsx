7fda3b24e2cdad89951b1c330f2f2fe99654696f
yui62yui <you@example.com>
2023-08-08 19:28:48 +0900

    Remove / Merge를 위해 Header 삭제 #2

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,55 +1,7 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
-  React.useState(null);
-  return (
-    <Box>
-      <AppBar position="fixed" sx={{ backgroundColor: '#000' }}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography
-            variant="h7"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' }, cursor: 'pointer' }}
-          >
-            커뮤니티 페이지
-          </Typography>
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' }, cursor: 'pointer' }}
-          >
-            페이지명
-            {/* 각 페이지마다 다른 이름이 오도록 해야함 */}
-          </Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography
-              variant="h7"
-              noWrap
-              component="div"
-              sx={{ display: { xs: 'none', sm: 'block' }, cursor: 'pointer' }}
-            >
-              로그아웃
-            </Typography>
-            <Link to="id">
-              {/* `/${user.id}` 등 유저 id 페이지로 이동*/}
-              <IconButton size="large" edge="end">
-                <AccountCircle sx={{ color: '#fff' }} />
-              </IconButton>
-            </Link>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  return <div></div>;
 };
 
 export default Header;
