40a2f0c00986a65ff40591756eae9f20df2e2fa8
yui62yui <you@example.com>
2023-08-11 14:55:01 +0900

    Style / 내 보관함 리스트 스크롤 효과 추가 #2

--- src/components/SavedPage.jsx
+++ src/components/SavedPage.jsx
@@ -64,38 +64,69 @@ export default SavedPage;
 
 const Container = styled.div`
   width: 80%;
-  min-width: 1000px;
   display: flex;
-  gap: 80px;
   margin: 0 auto;
 `;
 
 const LeftContainer = styled.div`
   width: 100%;
+  padding-right: 30px;
 `;
 
 const RightContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 320px));
-  gap: 30px;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 300px));
+  column-gap: 20px;
+  justify-content: center;
+  row-gap: 30px;
   width: 100%;
-  height: 100%;
+  height: 600px;
+  overflow: auto;
+  padding-right: 30px;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #eee;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #575757;
+  }
+`;
+
+const ListBoxContainer = styled.div`
+  position: relative;
+
+  & > button {
+    position: absolute;
+    bottom: 20px;
+    left: 0px;
+    right: 0px;
+    margin: 0 auto;
+  }
 `;
 
 const ListBox = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  width: 300px;
   height: 200px;
   max-height: 200px;
-  padding: 5px 30px;
+  padding: 40px 30px 20px;
   border-radius: 10px;
   background: center / cover no-repeat url(${ListBackgroundImg});
   cursor: pointer;
 
   & > p {
-    box-sizing: border-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+
     font-size: 16px;
     font-weight: 400;
     letter-spacing: -0.5px;
